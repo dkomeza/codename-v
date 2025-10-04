14964ca017b5b95984680863ec4d82f84385db9b
Dawid Komęza <dawid.komeza@gmail.com>
2025-10-05 00:07:23 +0200

    ✨ Add /admin/users

--- backend/src/routers/admin.router.ts
+++ backend/src/routers/admin.router.ts
@@ -1,4 +1,4 @@
-import { getAdminDashboardData, getCoordinators, getOrgs } from "@/controllers/admin.controller";
+import { getAdminDashboardData, getUsers } from "@/controllers/admin.controller";
 import { authenticate, authenticateAdmin } from "@/middleware/auth.middleware";
 import { Router } from "express";
 
@@ -9,8 +9,20 @@ adminRouter.use(authenticateAdmin);
 export default adminRouter;
 
 adminRouter.get("/", async (req, res) => {
-    const pageSize = 20;
+  const pageSize = 20;
 
   const data = await getAdminDashboardData(pageSize);
   res.status(200).json({ message: "Admin route accessed successfully." });
+});
+
+adminRouter.get("/users", async (req, res) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 50;
+
+  try {
+    const data = await getUsers(page, limit);
+    res.status(200).json(data);
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
 });
