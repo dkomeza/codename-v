1df3e98a1dd9829f5a228792da6ea2698fe16e57
Dawid Komęza <dawid.komeza@gmail.com>
2025-10-05 04:47:21 +0200

    ✅ Improve test coverage

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,38 +1,9 @@
-import { type NextFunction, type Request, type Response } from "express";
 import * as jwt from "jsonwebtoken";
 
 const JWT_SECRET = Bun.env.JWT_SECRET || "secret";
-
-type JWTPayload = {
-  id: string;
-};
 
 export const signJWT = (userId: string) => {
   return jwt.sign({ id: userId }, JWT_SECRET, {
     expiresIn: 86400,
   });
-};
-
-export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (res.locals.type !== 0) {
-    res.status(403).json({ message: "You are forbidden to access this endpoint!" });
-  }
-
-  next();
-};
-
-export const isOrganiser = (req: Request, res: Response, next: NextFunction) => {
-  if (res.locals.type !== 1) {
-    res.status(403).json({ message: "You are forbidden to access this endpoint!" });
-  }
-
-  next();
-};
-
-export const isCoordinator = (req: Request, res: Response, next: NextFunction) => {
-  if (res.locals.type !== 2) {
-    res.status(403).json({ message: "You are forbidden to access this endpoint!" });
-  }
-
-  next();
 };
